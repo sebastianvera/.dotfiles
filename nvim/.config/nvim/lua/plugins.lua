b6a404ed008258034df8f6077c24d94cb95b8930
Sebastian Vera <u.verainf@gmail.com>
2022-12-31 21:02:24 -0800

    Update lsp config, make it simpler by using lsp-zero

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -55,41 +55,35 @@ require("packer").startup(function(use)
 
   use { "junegunn/vim-easy-align" }
 
-  use {
-    "neovim/nvim-lspconfig",
-    "folke/neodev.nvim",
-    "onsails/lspkind-nvim",
+
+  -- use {
+  --   "folke/trouble.nvim",
+  --   requires = "kyazdani42/nvim-web-devicons",
+  -- }
+  --
+  -- use { "jose-elias-alvarez/null-ls.nvim" }
+   use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  requires = {
+		  -- LSP Support
+		  {'neovim/nvim-lspconfig'},
+		  {'williamboman/mason.nvim'},
+		  {'williamboman/mason-lspconfig.nvim'},
+
+		  -- Autocompletion
+		  {'hrsh7th/nvim-cmp'},
+		  {'hrsh7th/cmp-buffer'},
+		  {'hrsh7th/cmp-path'},
+		  {'saadparwaiz1/cmp_luasnip'},
+		  {'hrsh7th/cmp-nvim-lsp'},
+		  {'hrsh7th/cmp-nvim-lua'},
+
+		  -- Snippets
+		  {'L3MON4D3/LuaSnip'},
+		  {'rafamadriz/friendly-snippets'},
+	  }
   }
 
-  use {
-    "folke/trouble.nvim",
-    requires = "kyazdani42/nvim-web-devicons",
-  }
-
-  use { "jose-elias-alvarez/null-ls.nvim" }
-
-  use {
-    "hrsh7th/nvim-cmp",
-    requires = {
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-cmdline",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-nvim-lsp-signature-help",
-      "saadparwaiz1/cmp_luasnip",
-      "L3MON4D3/LuaSnip",
-      "windwp/nvim-autopairs",
-      "petertriho/cmp-git",
-    },
-  }
-
-  use {
-    "L3MON4D3/LuaSnip",
-    requires = {
-      "rafamadriz/friendly-snippets",
-      after = "LuaSnip",
-    },
-  }
 
   use { 'xiyaowong/nvim-transparent' }
   use { 'lewis6991/impatient.nvim' }
